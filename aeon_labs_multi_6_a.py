6d711ebc47091e4e6f26dcf1859890c788cefda8
PeterClaydon <peter.claydon@continuumbridge.com>
2015-10-28 16:35:14 +0000

    Changed timeout from 16 to 10 and enabled local wakeup

--- aeon_labs_multi_6_a.py
+++ aeon_labs_multi_6_a.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
-# aeo_labs_mullti_6_a.py
+# aeon_labs_mullti_6_a.py
 # Copyright (C) ContinuumBridge Limited, 2014-2015 - All Rights Reserved
 # Unauthorized copying of this file, via any medium is strictly prohibited
 # Proprietary and confidential
 # Written by Peter Claydon
 #
 BATTERY_CHECK_INTERVAL   = 21600    # How often to check battery (secs) - 6 hours
-MAX_INTERVAL             = 60*60
-MIN_INTERVAL             = 300
-WAKEUP_INTERVAL          = 60*30    # Wake up once every 30 minutes to allow reprogramming
+MAX_INTERVAL             = 3600
+MIN_INTERVAL             = 1800     # 
+CHECK_INTERVAL           = 7200     # If not update during this time, assume we've lost connection
+TIME_CUTOFF              = 1800     # Data older than this is considered "stale"
+POLL_INTERVAL            = 1800     # How often sensor should wake up      
 
 import sys
 import time
@@ -26,6 +28,7 @@ class Adaptor(CbAdaptor):
         self.state =            "stopped"
         self.apps =             {"binary_sensor": [],
                                  "battery": [],
+                                 "connected": [],
                                  "temperature": [],
                                  "humidity": [],
                                  "ultraviolet": [],
@@ -46,6 +49,8 @@ class Adaptor(CbAdaptor):
         self.lastBinaryTime =        0
         self.lastBatteryTime =       0
         self.lastUltravioletTime =   0
+        self.updateTime =            0
+        self.lastUpdateTime =        0
         # super's __init__ must be called:
         #super(Adaptor, self).__init__(argv)
         CbAdaptor.__init__(self, argv)
@@ -86,7 +91,17 @@ class Adaptor(CbAdaptor):
         else:
             self.cbLog("warning", "onAction. Unrecognised action: " +  str(action))
 
+    def checkConnected(self):
+        self.cbLog("debug", "checkConnected, time: " + str(time.time()) + ", updateTime: " + str(self.updateTime))
+        if time.time() - self.updateTime > CHECK_INTERVAL:
+            connected = False
+        else:
+            connected = True
+        self.sendCharacteristic("connected", connected, time.time())
+        reactor.callLater(CHECK_INTERVAL, self.checkConnected)
+
     def checkBattery(self):
+        self.cbLog("debug", "checkBattery")
         cmd = {"id": self.id,
                "request": "post",
                "address": self.addr,
@@ -97,32 +112,6 @@ class Adaptor(CbAdaptor):
               }
         self.sendZwaveMessage(cmd)
         reactor.callLater(BATTERY_CHECK_INTERVAL, self.checkBattery)
-
-    def pollSensors(self):
-        #self.cbLog("debug", "pollSensors")
-        if self.intervalChanged:
-            self.intervalChanged = False
-            # Set wakeup time
-            cmd = {"id": self.id,
-                   "request": "post",
-                   "address": self.addr,
-                   "instance": "0",
-                   "commandClass": "132",
-                   "action": "Set",
-                   "value": str(self.pollInterval) + ",1"
-                  }
-            self.sendZwaveMessage(cmd)
-        # Don't actually poll sensors as we've set them to auto-report on change
-        #cmd = {"id": self.id,
-        #       "request": "post",
-        #       "address": self.addr,
-        #       "instance": "0",
-        #       "commandClass": "49",
-        #       "action": "Get",
-        #       "value": ""
-        #      }
-        #self.sendZwaveMessage(cmd)
-        reactor.callLater(self.pollInterval, self.pollSensors)
 
     def onZwaveMessage(self, message):
         #self.cbLog("debug", "onZwaveMessage, message: " + str(message))
@@ -209,14 +198,24 @@ class Adaptor(CbAdaptor):
                    "value": "5,2,1"
                   }
             self.sendZwaveMessage(cmd)
-            # 16 s timeout period of no-motion detected before the Multisensor sends the OFF state after being triggered.
+            # Allow device to be woken up locally
+            cmd = {"id": self.id,
+                   "request": "post",
+                   "address": self.addr,
+                   "instance": "0",
+                   "commandClass": "132",
+                   "action": "Set",
+                   "value": "2,1,1"
+                  }
+            self.sendZwaveMessage(cmd)
+            # 10 s timeout period of no-motion detected before the Multisensor sends the OFF state after being triggered.
             cmd = {"id": self.id,
                    "request": "post",
                    "address": self.addr,
                    "instance": "0",
                    "commandClass": "112",
                    "action": "Set",
-                   "value": "3,16,2"
+                   "value": "3,10,2"
                   }
             self.sendZwaveMessage(cmd)
             # Automatically report parameters when they change
@@ -259,48 +258,71 @@ class Adaptor(CbAdaptor):
                    "value": "101,227,4"
                   }
             self.sendZwaveMessage(cmd)
-            reactor.callLater(10, self.checkBattery)
-            reactor.callLater(10, self.pollSensors)
+            # Wakeup interval
+            cmd = {"id": self.id,
+                   "request": "post",
+                   "address": self.addr,
+                   "instance": "0",
+                   "commandClass": "132",
+                   "action": "Set",
+                   "value": str(POLL_INTERVAL) + ",1"
+                  }
+            self.sendZwaveMessage(cmd)
+            # Send a message on wakeup
+            cmd = {"id": self.id,
+                   "request": "get",
+                   "address": self.addr,
+                   "instance": "0",
+                   "commandClass": "132",
+                   "value": "lastWakeup"
+                  }
+            self.sendZwaveMessage(cmd)
+            reactor.callLater(300, self.checkBattery)
+            reactor.callLater(CHECK_INTERVAL, self.checkConnected)
         elif message["content"] == "data":
             try:
                 if message["commandClass"] == "49":
                     if message["value"] == "1":
                         updateTime = message["data"]["val"]["updateTime"] 
                         # Only send if we don't already have an update from this time and the update is recent (not stale after restart)
-                        if updateTime != self.lastTemperatureTime and time.time() - updateTime < 10:
+                        if updateTime != self.lastTemperatureTime and time.time() - updateTime < TIME_CUTOFF:
                             temperature = message["data"]["val"]["value"] 
                             if temperature is not None:
                                 self.cbLog("debug", "onZwaveMessage, temperature: " + str(temperature))
                                 self.sendCharacteristic("temperature", temperature, time.time())
                                 self.lastTemperatureTime = updateTime
+                                self.updateTime = updateTime
                     elif message["value"] == "3":
                         updateTime = message["data"]["val"]["updateTime"] 
-                        if updateTime != self.lastLuminanceTime and time.time() - updateTime < 10:
+                        if updateTime != self.lastLuminanceTime and time.time() - updateTime < TIME_CUTOFF:
                             luminance = message["data"]["val"]["value"] 
                             if luminance is not None:
                                 self.cbLog("debug", "onZwaveMessage, luminance: " + str(luminance))
                                 self.sendCharacteristic("luminance", luminance, time.time())
                                 self.lastLuminanceTime = updateTime
+                                self.updateTime = updateTime
                     elif message["value"] == "5":
                         updateTime = message["data"]["val"]["updateTime"] 
-                        if updateTime != self.lastHumidityTime and time.time() - updateTime < 10:
+                        if updateTime != self.lastHumidityTime and time.time() - updateTime < TIME_CUTOFF:
                             humidity = message["data"]["val"]["value"] 
                             if humidity is not None:
                                 self.cbLog("debug", "onZwaveMessage, humidity: " + str(humidity))
                                 self.sendCharacteristic("humidity", humidity, time.time())
                                 self.lastHumidityTime = updateTime
+                                self.updateTime = updateTime
                     elif message["value"] == "27":
                         updateTime = message["data"]["val"]["updateTime"] 
-                        if updateTime != self.lastUltravioletTime and time.time() - updateTime < 10:
+                        if updateTime != self.lastUltravioletTime and time.time() - updateTime < TIME_CUTOFF:
                             ultraviolet = message["data"]["val"]["value"] 
                             if ultraviolet is not None:
                                 self.cbLog("debug", "onZwaveMessage, ultraviolet: " + str(ultraviolet))
                                 self.sendCharacteristic("ultraviolet", ultraviolet, time.time())
                                 self.lastUltravioletTime = updateTime
+                                self.updateTime = updateTime
                 elif message["commandClass"] == "48":
                     if message["value"] == "1":
                         updateTime = message["data"]["level"]["updateTime"]
-                        if updateTime != self.lastBinaryTime and time.time() - updateTime < 10:
+                        if updateTime != self.lastBinaryTime and time.time() - updateTime < TIME_CUTOFF:
                             if message["data"]["level"]["value"]:
                                 b = "on"
                             else:
@@ -308,17 +330,22 @@ class Adaptor(CbAdaptor):
                             self.cbLog("debug", "onZwaveMessage, alarm: " + b)
                             self.sendCharacteristic("binary_sensor", b, time.time())
                             self.lastBinaryTime = updateTime
+                            self.updateTime = updateTime
                 elif message["commandClass"] == "128":
                     updateTime = message["data"]["last"]["updateTime"]
-                    if updateTime != self.lastBatteryTime and time.time() - updateTime < 10:
+                    if updateTime != self.lastBatteryTime and time.time() - updateTime < TIME_CUTOFF:
                         battery = message["data"]["last"]["value"] 
+                        self.cbLog("debug", "battery: " + str(battery))
                         msg = {"id": self.id,
                                "status": "battery_level",
                                "battery_level": battery}
                         self.sendManagerMessage(msg)
                         self.sendCharacteristic("battery", battery, time.time())
                         self.lastBatteryTime = updateTime
-                self.updateTime = message["data"]["updateTime"]
+                        self.updateTime = updateTime
+                elif message["commandClass"] == "132":
+                    self.cbLog("debug", "device woke up")
+                    self.updateTime = message["data"]["updateTime"]
             except Exception as ex:
                 self.cbLog("warning", "onZwaveMessage, unexpected message: " + str(message))
                 self.cbLog("warning", "Exception: " + str(type(ex)) + str(ex.args))
@@ -333,6 +360,7 @@ class Adaptor(CbAdaptor):
                             {"characteristic": "luminance", "interval": 0},
                             {"characteristic": "humidity", "interval": 0},
                             {"characteristic": "ultraviolet", "interval": 0},
+                            {"characteristic": "connected", "interval": 0},
                             {"characteristic": "battery", "interval": BATTERY_CHECK_INTERVAL}],
                 "content": "service"}
         self.sendMessage(resp, message["id"])
@@ -349,6 +377,7 @@ class Adaptor(CbAdaptor):
             if message["id"] not in self.apps[f["characteristic"]]:
                 self.apps[f["characteristic"]].append(message["id"])
                 if f["characteristic"] != "battery" and f["characteristic"] != "binary_sensor":
+                    # This is now redundant for this sensor as it is not polled
                     if "interval" in f:
                         if f["interval"] <= MIN_INTERVAL:
                             self.intervals[f["characteristic"]] = MIN_INTERVAL
